c7614244d5a15f6cbbfbd090779924b6fb81c446
Claudio Moreno <102330321+yasperht@users.noreply.github.com>
2023-06-25 23:58:58 -0500

    feat: creating animations and transitions to the entire page.

---
+++ js/index.js
@@ -0,0 +1,81 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.principal', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.benefits', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    });
+
+    sr.reveal('.acerca-de-la-app', {
+        duration: 4000,
+        origin: 'right',
+        distance: '400px'
+    });
+
+    sr.reveal('.testimonios-de-clientes', {
+        duration: 4000,
+        origin: 'left',
+        distance: '400px'
+    });
+
+    sr.reveal('.contactanos', {
+        duration: 3000,
+        origin: 'right',
+        distance: '400px'
+    });
+ 
+
+    sr.reveal('.sobre-nosotros', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    
+
+      sr.reveal('.hero-title', {
+        duration: 2000,
+        origin: 'left',
+        distance: '200px',
+        delay: 500
+      });
+  
+      sr.reveal('.hero-description', {
+        duration: 2000,
+        origin: 'right',
+        distance: '200px',
+        delay: 500
+      });
+  
+      sr.reveal('.cta-button', {
+        duration: 1500,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 1000
+      });
+  
+      sr.reveal('.feature', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px',
+        delay: 200
+      }); 
+  
+      sr.reveal('.contact-info', {
+        duration: 1500,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 500
+      });
