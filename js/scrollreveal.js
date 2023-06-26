e68a6c0e297ac3e789233d61db1410cd3107c855
JanoverSaldana <89089838+JanoverSaldana@users.noreply.github.com>
2023-06-26 00:23:03 -0500

    feat: create custom scrollreveal style part 2

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -292,3 +292,215 @@
 
         each(sequenceIds.stale, function (staleId) { return delete this$1.store.sequences[staleId]; });
     }
+
+/*! @license Rematrix v0.3.0
+
+        Copyright 2018 Julian Lloyd.
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in
+        all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+        THE SOFTWARE.
+    */
+    /**
+     * @module Rematrix
+     */
+
+    /**
+     * Transformation matrices in the browser come in two flavors:
+     *
+     *  - `matrix` using 6 values (short)
+     *  - `matrix3d` using 16 values (long)
+     *
+     * This utility follows this [conversion guide](https://goo.gl/EJlUQ1)
+     * to expand short form matrices to their equivalent long form.
+     *
+     * @param  {array} source - Accepts both short and long form matrices.
+     * @return {array}
+     */
+    function format(source) {
+        if (source.constructor !== Array) {
+            throw new TypeError('Expected array.')
+        }
+        if (source.length === 16) {
+            return source
+        }
+        if (source.length === 6) {
+            var matrix = identity();
+            matrix[0] = source[0];
+            matrix[1] = source[1];
+            matrix[4] = source[2];
+            matrix[5] = source[3];
+            matrix[12] = source[4];
+            matrix[13] = source[5];
+            return matrix
+        }
+        throw new RangeError('Expected array with either 6 or 16 values.')
+    }
+
+    /**
+     * Returns a matrix representing no transformation. The product of any matrix
+     * multiplied by the identity matrix will be the original matrix.
+     *
+     * > **Tip:** Similar to how `5 * 1 === 5`, where `1` is the identity.
+     *
+     * @return {array}
+     */
+    function identity() {
+        var matrix = [];
+        for (var i = 0; i < 16; i++) {
+            i % 5 == 0 ? matrix.push(1) : matrix.push(0);
+        }
+        return matrix
+    }
+
+    /**
+     * Returns a 4x4 matrix describing the combined transformations
+     * of both arguments.
+     *
+     * > **Note:** Order is very important. For example, rotating 45°
+     * along the Z-axis, followed by translating 500 pixels along the
+     * Y-axis... is not the same as translating 500 pixels along the
+     * Y-axis, followed by rotating 45° along on the Z-axis.
+     *
+     * @param  {array} m - Accepts both short and long form matrices.
+     * @param  {array} x - Accepts both short and long form matrices.
+     * @return {array}
+     */
+    function multiply(m, x) {
+        var fm = format(m);
+        var fx = format(x);
+        var product = [];
+
+        for (var i = 0; i < 4; i++) {
+            var row = [fm[i], fm[i + 4], fm[i + 8], fm[i + 12]];
+            for (var j = 0; j < 4; j++) {
+                var k = j * 4;
+                var col = [fx[k], fx[k + 1], fx[k + 2], fx[k + 3]];
+                var result =
+                    row[0] * col[0] + row[1] * col[1] + row[2] * col[2] + row[3] * col[3];
+
+                product[i + k] = result;
+            }
+        }
+
+        return product
+    }
+
+    /**
+     * Attempts to return a 4x4 matrix describing the CSS transform
+     * matrix passed in, but will return the identity matrix as a
+     * fallback.
+     *
+     * > **Tip:** This method is used to convert a CSS matrix (retrieved as a
+     * `string` from computed styles) to its equivalent array format.
+     *
+     * @param  {string} source - `matrix` or `matrix3d` CSS Transform value.
+     * @return {array}
+     */
+    function parse(source) {
+        if (typeof source === 'string') {
+            var match = source.match(/matrix(3d)?\(([^)]+)\)/);
+            if (match) {
+                var raw = match[2].split(', ').map(parseFloat);
+                return format(raw)
+            }
+        }
+        return identity()
+    }
+
+    /**
+     * Returns a 4x4 matrix describing X-axis rotation.
+     *
+     * @param  {number} angle - Measured in degrees.
+     * @return {array}
+     */
+    function rotateX(angle) {
+        var theta = Math.PI / 180 * angle;
+        var matrix = identity();
+
+        matrix[5] = matrix[10] = Math.cos(theta);
+        matrix[6] = matrix[9] = Math.sin(theta);
+        matrix[9] *= -1;
+
+        return matrix
+    }
+
+    /**
+     * Returns a 4x4 matrix describing Y-axis rotation.
+     *
+     * @param  {number} angle - Measured in degrees.
+     * @return {array}
+     */
+    function rotateY(angle) {
+        var theta = Math.PI / 180 * angle;
+        var matrix = identity();
+
+        matrix[0] = matrix[10] = Math.cos(theta);
+        matrix[2] = matrix[8] = Math.sin(theta);
+        matrix[2] *= -1;
+
+        return matrix
+    }
+
+    /**
+     * Returns a 4x4 matrix describing Z-axis rotation.
+     *
+     * @param  {number} angle - Measured in degrees.
+     * @return {array}
+     */
+    function rotateZ(angle) {
+        var theta = Math.PI / 180 * angle;
+        var matrix = identity();
+
+        matrix[0] = matrix[5] = Math.cos(theta);
+        matrix[1] = matrix[4] = Math.sin(theta);
+        matrix[4] *= -1;
+
+        return matrix
+    }
+
+    /**
+     * Returns a 4x4 matrix describing 2D scaling. The first argument
+     * is used for both X and Y-axis scaling, unless an optional
+     * second argument is provided to explicitly define Y-axis scaling.
+     *
+     * @param  {number} scalar    - Decimal multiplier.
+     * @param  {number} [scalarY] - Decimal multiplier.
+     * @return {array}
+     */
+    function scale(scalar, scalarY) {
+        var matrix = identity();
+
+        matrix[0] = scalar;
+        matrix[5] = typeof scalarY === 'number' ? scalarY : scalar;
+
+        return matrix
+    }
+
+    /**
+     * Returns a 4x4 matrix describing X-axis translation.
+     *
+     * @param  {number} distance - Measured in pixels.
+     * @return {array}
+     */
+    function translateX(distance) {
+        var matrix = identity();
+        matrix[12] = distance;
+        return matrix
+    }
+
+                     
