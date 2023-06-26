157c67efedc6607deb5e9f07f6bb9502c5f55782
Claudio Moreno <102330321+yasperht@users.noreply.github.com>
2023-06-26 00:25:14 -0500

    feat: create custom scrollreveal style part 3

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -503,4 +503,254 @@
         return matrix
     }
 
+     /**
+     * Returns a 4x4 matrix describing Y-axis translation.
+     *
+     * @param  {number} distance - Measured in pixels.
+     * @return {array}
+     */
+    function translateY(distance) {
+        var matrix = identity();
+        matrix[13] = distance;
+        return matrix
+    }
+
+    var getPrefixedCssProp = (function () {
+        var properties = {};
+        var style = document.documentElement.style;
+
+        function getPrefixedCssProperty(name, source) {
+            if ( source === void 0 ) source = style;
+
+            if (name && typeof name === 'string') {
+                if (properties[name]) {
+                    return properties[name]
+                }
+                if (typeof source[name] === 'string') {
+                    return (properties[name] = name)
+                }
+                if (typeof source[("-webkit-" + name)] === 'string') {
+                    return (properties[name] = "-webkit-" + name)
+                }
+                throw new RangeError(("Unable to find \"" + name + "\" style property."))
+            }
+            throw new TypeError('Expected a string.')
+        }
+
+        getPrefixedCssProperty.clearCache = function () { return (properties = {}); };
+
+        return getPrefixedCssProperty
+    })();
+
+    function style(element) {
+        var computed = window.getComputedStyle(element.node);
+        var position = computed.position;
+        var config = element.config;
+
+        /**
+         * Generate inline styles
+         */
+        var inline = {};
+        var inlineStyle = element.node.getAttribute('style') || '';
+        var inlineMatch = inlineStyle.match(/[\w-]+\s*:\s*[^;]+\s*/gi) || [];
+
+        inline.computed = inlineMatch ? inlineMatch.map(function (m) { return m.trim(); }).join('; ') + ';' : '';
+
+        inline.generated = inlineMatch.some(function (m) { return m.match(/visibility\s?:\s?visible/i); })
+            ? inline.computed
+            : inlineMatch.concat( ['visibility: visible']).map(function (m) { return m.trim(); }).join('; ') + ';';
+
+        /**
+         * Generate opacity styles
+         */
+        var computedOpacity = parseFloat(computed.opacity);
+        var configOpacity = !isNaN(parseFloat(config.opacity))
+            ? parseFloat(config.opacity)
+            : parseFloat(computed.opacity);
+
+        var opacity = {
+            computed: computedOpacity !== configOpacity ? ("opacity: " + computedOpacity + ";") : '',
+            generated: computedOpacity !== configOpacity ? ("opacity: " + configOpacity + ";") : ''
+        };
+
+        /**
+         * Generate transformation styles
+         */
+        var transformations = [];
+
+        if (parseFloat(config.distance)) {
+            var axis = config.origin === 'top' || config.origin === 'bottom' ? 'Y' : 'X';
+
+            /**
+             * Let’s make sure our our pixel distances are negative for top and left.
+             * e.g. { origin: 'top', distance: '25px' } starts at `top: -25px` in CSS.
+             */
+            var distance = config.distance;
+            if (config.origin === 'top' || config.origin === 'left') {
+                distance = /^-/.test(distance) ? distance.substr(1) : ("-" + distance);
+            }
+
+            var ref = distance.match(/(^-?\d+\.?\d?)|(em$|px$|%$)/g);
+            var value = ref[0];
+            var unit = ref[1];
+
+            switch (unit) {
+                case 'em':
+                    distance = parseInt(computed.fontSize) * value;
+                    break
+                case 'px':
+                    distance = value;
+                    break
+                case '%':
+                    /**
+                     * Here we use `getBoundingClientRect` instead of
+                     * the existing data attached to `element.geometry`
+                     * because only the former includes any transformations
+                     * current applied to the element.
+                     *
+                     * If that behavior ends up being unintuitive, this
+                     * logic could instead utilize `element.geometry.height`
+                     * and `element.geoemetry.width` for the distance calculation
+                     */
+                    distance =
+                        axis === 'Y'
+                            ? (element.node.getBoundingClientRect().height * value) / 100
+                            : (element.node.getBoundingClientRect().width * value) / 100;
+                    break
+                default:
+                    throw new RangeError('Unrecognized or missing distance unit.')
+            }
+
+            if (axis === 'Y') {
+                transformations.push(translateY(distance));
+            } else {
+                transformations.push(translateX(distance));
+            }
+        }
+
+        if (config.rotate.x) { transformations.push(rotateX(config.rotate.x)); }
+        if (config.rotate.y) { transformations.push(rotateY(config.rotate.y)); }
+        if (config.rotate.z) { transformations.push(rotateZ(config.rotate.z)); }
+        if (config.scale !== 1) {
+            if (config.scale === 0) {
+                /**
+                 * The CSS Transforms matrix interpolation specification
+                 * basically disallows transitions of non-invertible
+                 * matrixes, which means browsers won't transition
+                 * elements with zero scale.
+                 *
+                 * That’s inconvenient for the API and developer
+                 * experience, so we simply nudge their value
+                 * slightly above zero; this allows browsers
+                 * to transition our element as expected.
+                 *
+                 * `0.0002` was the smallest number
+                 * that performed across browsers.
+                 */
+                transformations.push(scale(0.0002));
+            } else {
+                transformations.push(scale(config.scale));
+            }
+        }
+
+        var transform = {};
+        if (transformations.length) {
+            transform.property = getPrefixedCssProp('transform');
+            /**
+             * The default computed transform value should be one of:
+             * undefined || 'none' || 'matrix()' || 'matrix3d()'
+             */
+            transform.computed = {
+                raw: computed[transform.property],
+                matrix: parse(computed[transform.property])
+            };
+
+            transformations.unshift(transform.computed.matrix);
+            var product = transformations.reduce(multiply);
+
+            transform.generated = {
+                initial: ((transform.property) + ": matrix3d(" + (product.join(', ')) + ");"),
+                final: ((transform.property) + ": matrix3d(" + (transform.computed.matrix.join(', ')) + ");")
+            };
+        } else {
+            transform.generated = {
+                initial: '',
+                final: ''
+            };
+        }
+
+        /**
+         * Generate transition styles
+         */
+        var transition = {};
+        if (opacity.generated || transform.generated.initial) {
+            transition.property = getPrefixedCssProp('transition');
+            transition.computed = computed[transition.property];
+            transition.fragments = [];
+
+            var delay = config.delay;
+            var duration = config.duration;
+            var easing = config.easing;
+
+            if (opacity.generated) {
+                transition.fragments.push({
+                    delayed: ("opacity " + (duration / 1000) + "s " + easing + " " + (delay / 1000) + "s"),
+                    instant: ("opacity " + (duration / 1000) + "s " + easing + " 0s")
+                });
+            }
+
+            if (transform.generated.initial) {
+                transition.fragments.push({
+                    delayed: ((transform.property) + " " + (duration / 1000) + "s " + easing + " " + (delay / 1000) + "s"),
+                    instant: ((transform.property) + " " + (duration / 1000) + "s " + easing + " 0s")
+                });
+            }
+
+            /**
+             * The default computed transition property should be undefined, or one of:
+             * '' || 'none 0s ease 0s' || 'all 0s ease 0s' || 'all 0s 0s cubic-bezier()'
+             */
+            var hasCustomTransition =
+                transition.computed && !transition.computed.match(/all 0s|none 0s/);
+
+            if (hasCustomTransition) {
+                transition.fragments.unshift({
+                    delayed: transition.computed,
+                    instant: transition.computed
+                });
+            }
+
+            var composed = transition.fragments.reduce(
+                function (composition, fragment, i) {
+                    composition.delayed += i === 0 ? fragment.delayed : (", " + (fragment.delayed));
+                    composition.instant += i === 0 ? fragment.instant : (", " + (fragment.instant));
+                    return composition
+                },
+                {
+                    delayed: '',
+                    instant: ''
+                }
+            );
+
+            transition.generated = {
+                delayed: ((transition.property) + ": " + (composed.delayed) + ";"),
+                instant: ((transition.property) + ": " + (composed.instant) + ";")
+            };
+        } else {
+            transition.generated = {
+                delayed: '',
+                instant: ''
+            };
+        }
+
+        return {
+            inline: inline,
+            opacity: opacity,
+            position: position,
+            transform: transform,
+            transition: transition
+        }
+    }                
+                     
+
                      
